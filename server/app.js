0f10cde246f5a7071c748a5cd867a9bba8d21d1f
Seid-Mohamme <seidmamme@gmail.com>
2025-06-28 19:56:38 +0300

    app js updated

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(
       "http://localhost:4321",
       "http://localhost:5173",
       "http://localhost:3000",
-      "https://evangadi-forum-iota-eosin.vercel.app/"
+      "https://evangadi-forum-eight.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
@@ -83,10 +83,6 @@ app.use(globalErrorHandler);
 // Start server and test database connection
 async function startServer() {
   try {
-<<<<<<< HEAD
-    await dbconnection.query("SELECT 'test'");
-=======
->>>>>>> upstream/main
     app.listen(process.env.PORT || 5400, () => {
       console.log(
         `Server is running on: http://localhost:${process.env.PORT || 5400}`
